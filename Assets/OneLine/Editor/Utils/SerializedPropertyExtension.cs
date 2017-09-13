de050a91b0742e22c3167bf9667bba0aefe7530e
slavniyteo <slavniyteo@gmail.com>
2017-09-13 09:02:46 +0300

    some extensions

--- Assets/OneLine/Editor/Utils/SerializedPropertyExtension.cs
+++ Assets/OneLine/Editor/Utils/SerializedPropertyExtension.cs
@@ -106,5 +106,16 @@ namespace OneLine {
             return path.Substring(path.Length - 3, 3) == "[0]";
         }
 
+        public static IEnumerable<TResult> Merge<TFirst, TSecond, TResult> (this IEnumerable<TFirst> first,
+                                                                            IEnumerable<TSecond> second,
+                                                                            Func<TFirst, TSecond, TResult> selector){
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+            while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext()){
+
+                yield return selector(firstEnumerator.Current, secondEnumerator.Current);
+            }
+        }
+
     }
 }
