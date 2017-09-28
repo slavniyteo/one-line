53008c83080bb4ab50c4faeab9ef06d7cf831d16
slavniyteo <slavniyteo@gmail.com>
2017-09-28 16:52:39 +0300

    README and examples

--- Assets/OneLine/Example/Details/Scripts/WidthExample.cs
+++ Assets/OneLine/Example/Details/Scripts/WidthExample.cs
@@ -5,15 +5,23 @@ using OneLine;
 [CreateAssetMenu(menuName = "OneLine/WidthExample")]
 public class WidthExample : ScriptableObject {
     [SerializeField, OneLine]
-    private WidthAndWeight fixedWidth;
+    private RootField root;
 
     [Serializable]
-    public class WidthAndWeight {
-        [SerializeField, Width(75)]
+    public class RootField {
+        [SerializeField, Width(70)]
         private string first;
-        [SerializeField]
+        [SerializeField] // by default width = 0
         private string second;
         [SerializeField, Weight(2), Width(25)]
         private string third;
+        [SerializeField, Width(10000000)]
+        private NestedField forth;
+    }
+
+    [Serializable]
+    public class NestedField {
+        [SerializeField]
+        private string first;
     }
 }
