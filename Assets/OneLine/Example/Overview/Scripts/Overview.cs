e1606dc99c69d261b938053161a5c163daa92a0b
slavniyteo <slavniyteo@gmail.com>
2017-10-19 13:06:44 +0300

    New Overview: Width and Customize blocks

--- Assets/OneLine/Example/Overview/Scripts/Overview.cs
+++ Assets/OneLine/Example/Overview/Scripts/Overview.cs
@@ -53,11 +53,11 @@ namespace OneLine.Examples {
         }
         [Serializable]
         public class OneLineWithTooltip {
-            [SerializeField, Weight(0.7f)]
+            [SerializeField, Weight(0.8f)]
             private string first;
             [SerializeField, Tooltip("I'm here -- tooltip on the second NESTED FIELD!")]
             private string second;
-            [SerializeField, Weight(0.9f)]
+            [SerializeField]
             private string third;
         }
 #endregion
@@ -78,84 +78,65 @@ public class ThreeFields{
     public string third;
 }
 
-#region Weights
+#region Weight and Width
         [Space, Separator("[ Weight Attribute ]")]
         [SerializeField, OneLine]
-        private Weights1 differentWeights1;
+        private WidthFirstLine oneLineCalculatesFieldsWidthesBasedOnAttributes;
         [SerializeField, OneLine]
-        private Weights2 differentWeights2;
-
-        [Serializable]
-        public class Weights1 {
-            [SerializeField, Weight(3)]
-            private int first;
-            [SerializeField, Weight(2)]
-            private int second;
-            [SerializeField, Weight(1)]
-            private int third;
-        }
-        [Serializable]
-        public class Weights2 {
-            [SerializeField, Weight(5)]
-            private int first;
-            [SerializeField, Weight(1)]
-            private int second;
-        }
-#endregion
-
-#region Width
-        [Separator("[ Width Attribute ]")]
+        private WidthWeightAttribute weightAttributeDeterminesRelativeWidth;
         [SerializeField, OneLine]
-        private WidthAndWeight fixedWidth;
-        [SerializeField, OneLine(Header = LineHeader.Short)]
-        private WidthOnly onlyFixedWidth;
+        private WidthWidthAttribute widthAttributeDeterminesAdditionalFixedWidth;
 
         [Serializable]
-        public class WidthOnly {
-            [SerializeField, Width(50)]
+        public class WidthFirstLine {
+            [SerializeField]
             private string first;
-            [SerializeField, Width(75)]
-            private string second;
         }
         [Serializable]
-        public class WidthAndWeight {
-            [SerializeField, Width(50)]
+        public class WidthWeightAttribute {
+            [SerializeField, Weight(3)]
+            private string first;
+            [SerializeField, Weight(2)]
+            private string second;
+            [SerializeField, Weight(1)]
+            private string third;
+        }
+        [Serializable]
+        public class WidthWidthAttribute {
+            [SerializeField, Width(75)]
             private string first;
             [SerializeField]
             private string second;
-            [SerializeField, Width(75)]
+            [SerializeField, Width(50)]
             private string third;
         }
 #endregion
 
 #region Hide Label
-        [Separator("[ Hide Label Attribute ]")]
+        [Space, Separator("[ Customize you database ]")]
+        [SerializeField]
+        private string youCanCustomizeYouDatabaseByAttributes;
         [SerializeField, OneLine, HideLabel]
-        private OneField oneFieldHidesLabel;
-        [SerializeField, OneLine, HideLabel]
-        private TwoFields twoFieldsHidesLabel;
-#endregion
-
-#region Highlight
-        [Separator("[ Highlight Attribute ]")]
-        [SerializeField, OneLine, Highlight]
-        private TwoFields highlightedRootField;
-        [SerializeField, OneLine, Highlight, HideLabel]
-        private ThreeFields highlightAndHideLabel;
+        private CustomizeHideLabel hideLabel;
         [SerializeField, OneLine]
-        private HighlightedFields highlightedFields;
-        [SerializeField, OneLine]
-        private HighlightedFields[] arrayWithHighlightedFields;
+        private HighlightedFields highlightAttributeHelpsToPointOnMostImportantThings;
 
         [Serializable]
-        public class HighlightedFields {
-            [SerializeField, Highlight(0, 1, 0)]
-            private string first;
-            [SerializeField, Highlight(0, 0, 1)]
-            private string second;
+        public class CustomizeHideLabel {
             [SerializeField]
+            private string first;
+            [SerializeField]
+            private string second;
+        }
+        [Serializable]
+        public class HighlightedFields {
+            [SerializeField]
+            private string first;
+            [SerializeField]
+            private string second;
+            [SerializeField, Highlight(0, 1, 0)]
             private string third;
-            [SerializeField, Highlight(1, 1, 0)]
+            [SerializeField]
             private string fourth;
         }
 #endregion
