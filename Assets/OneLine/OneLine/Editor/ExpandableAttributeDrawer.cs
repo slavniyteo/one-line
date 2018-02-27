3b39537b291d8a6491e6af9a6e795cc11fca59b1
slavniyteo <slavniyteo@gmail.com>
2018-02-27 12:31:39 +0300

    expandable: draw in popup simply

--- Assets/OneLine/OneLine/Editor/ExpandableAttributeDrawer.cs
+++ Assets/OneLine/OneLine/Editor/ExpandableAttributeDrawer.cs
@@ -9,185 +9,90 @@ namespace OneLine {
     [CustomPropertyDrawer(typeof(ExpandableAttribute))]
     public class ExpandableAttributeDrawer : PropertyDrawer {
 
-            // Use the following area to change the style of the expandable ScriptableObject drawers;
-        #region Style Setup
-        private enum BackgroundStyles {
-            None,
-            HelpBox,
-            Darken,
-            Lighten
-        }
+        private GUIStyle foldoutStyle;
     
-        /// <summary>
-        /// Whether the default editor Script field should be shown.
-        /// </summary>
-        private static bool SHOW_SCRIPT_FIELD = false;
+        public override void OnGUI (Rect rect, SerializedProperty property, GUIContent label) {
+            rect = EditorGUI.PrefixLabel(rect, label);
+
+            var rects = rect.CutFromLeft(14, 0);
     
-        /// <summary>
-        /// The spacing on the inside of the background rect.
-        /// </summary>
-        private static float INNER_SPACING = 6.0f;
-    
-        /// <summary>
-        /// The spacing on the outside of the background rect.
-        /// </summary>
-        private static float OUTER_SPACING = 4.0f;
-    
-        /// <summary>
-        /// The style the background uses.
-        /// </summary>
-        private static BackgroundStyles BACKGROUND_STYLE = BackgroundStyles.HelpBox;
-    
-        /// <summary>
-        /// The colour that is used to darken the background.
-        /// </summary>
-        private static Color DARKEN_COLOUR = new Color (0.0f, 0.0f, 0.0f, 0.2f);
-    
-        /// <summary>
-        /// The colour that is used to lighten the background.
-        /// </summary>
-        private static Color LIGHTEN_COLOUR = new Color (1.0f, 1.0f, 1.0f, 0.2f);
-        #endregion
-    
-        // public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
-        // {
-        //     float totalHeight = 0.0f;
-    
-        //     totalHeight += EditorGUIUtility.singleLineHeight;
-    
-        //     if (property.objectReferenceValue == null)
-        //         return totalHeight;
-    
-        //     if (!property.isExpanded)
-        //         return totalHeight;
-    
-        //     SerializedObject targetObject = new SerializedObject (property.objectReferenceValue);
-    
-        //     if (targetObject == null)
-        //         return totalHeight;
-        
-        //     SerializedProperty field = targetObject.GetIterator ();
-    
-        //     field.NextVisible (true);
-    
-        //     if (SHOW_SCRIPT_FIELD)
-        //     {
-        //         totalHeight += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-        //     }
-    
-        //     while (field.NextVisible (false))
-        //     {
-        //         totalHeight += EditorGUI.GetPropertyHeight (field, true) + EditorGUIUtility.standardVerticalSpacing;
-        //     }
-    
-        //     totalHeight += INNER_SPACING * 2;
-        //     totalHeight += OUTER_SPACING * 2;
-    
-        //     return totalHeight;
-        // }
-    
-        public override void OnGUI (Rect rect, SerializedProperty property, GUIContent label)
-        {
-            var rects = rect.CutFromRight(20);
-    
-            EditorGUI.PropertyField (rects[0], property, label, true);
+            EditorGUI.PropertyField (rects[1], property, GUIContent.none, true);
     
             if (property.objectReferenceValue == null) return;
         
-            if (GUI.Button(rects[1], "E")){
-                PopupWindow.Show(rect, new ExpandedObjectWindow(rect, property));
+            DrawFoldout(rects[0], rects[1], property);
+        }
+
+        private void DrawFoldout(Rect foldoutRect, Rect propertyRect, SerializedProperty property){
+            if (foldoutStyle == null) {
+                foldoutStyle = new GUIStyle(EditorStyles.foldout);
+                foldoutStyle.active = foldoutStyle.normal;
             }
-    
+
+            if (GUI.Button(foldoutRect, "", foldoutStyle)){
+                PopupWindow.Show(propertyRect.MoveDownFor(0), new ExpandedObjectWindow(propertyRect, property));
+            }
         }
 
         public class ExpandedObjectWindow : PopupWindowContent {
-            private Rect rect;
+
+            private Vector2 size;
             private SerializedProperty property;
 
             public ExpandedObjectWindow(Rect rect, SerializedProperty property){
-                this.rect = rect;
-                this.rect.height = 300;
+                this.size = new Vector2(rect.width, 0);
                 this.property = property;
             }
 
             public override Vector2 GetWindowSize() {
-                return rect.size;
+                return size;
             }
 
             public override void OnGUI(Rect rect) {
-                DrawExpandedObject(rect, property);
+                DrawExpandedObject(rect, new SerializedObject(property.objectReferenceValue));
             }
 
-            public override void OnOpen() {
-            }
+            private void DrawExpandedObject(Rect rect, SerializedObject target) {
+                if (target == null) return; 
 
-            public override void OnClose() {
-            }
+                rect = rect.Intend(5).FirstLine();
 
-            private void DrawExpandedObject(Rect rect, SerializedProperty property) {
-                SerializedObject targetObject = new SerializedObject (property.objectReferenceValue);
+                DrawChildren(rect, target);
         
-                if (targetObject == null) {
-                    Debug.Log("Target object is null");
-                    return;
-                }
+                target.ApplyModifiedProperties();
+            }
 
-                // First line
-                rect = rect.CutFromTop(18)[0];
+            private void DrawChildren(Rect rect, SerializedObject target){
+                var property = target.GetIterator();
+                property.NextVisible (true);
 
-                int index = 0;
-                SerializedProperty field = targetObject.GetIterator ();
-                field.NextVisible (true);
-        
-                if (SHOW_SCRIPT_FIELD) {
-                    //Show the disabled script field
-                    EditorGUI.BeginDisabledGroup (true);
-                    EditorGUI.PropertyField (rect, field, true);
-                    EditorGUI.EndDisabledGroup ();
-                    index++;
-
+                if (property.name == "m_Script"){
+                    DrawScriptReference(rect, property);
                     rect = rect.MoveDown();
+                    property.NextVisible(false);
                 }
-        
-                //Replacement for "editor.OnInspectorGUI ();" so we have more control on how we draw the editor
-                while (field.NextVisible (false)) {
+                
+                do {
                     try {
-                        rect.height = EditorGUI.GetPropertyHeight(field, false);
-                        EditorGUI.PropertyField (rect, field, true);
+                        rect.height = EditorGUI.GetPropertyHeight(property, true);
+                        EditorGUI.PropertyField (rect, property, true);
                         rect.y += rect.height;
                     }
                     catch (StackOverflowException) {
-                        field.objectReferenceValue = null;
+                        property.objectReferenceValue = null;
                         Debug.LogError ("Detected self-nesting cauisng a StackOverflowException, avoid using the same " +
                             "object iside a nested structure.");
                     }
-        
-                    index++;
                 }
-        
-                targetObject.ApplyModifiedProperties ();
+                while (property.NextVisible (false));
+
+                size.y = rect.y + 16;
             }
 
-            /// <summary>
-            /// Draws the Background
-            /// </summary>
-            /// <param name="rect">The Rect where the background is drawn.</param>
-            private void DrawBackground (Rect rect)
-            {
-                switch (BACKGROUND_STYLE) {
-        
-                case BackgroundStyles.HelpBox:
-                    EditorGUI.HelpBox (rect, "", MessageType.None);
-                    break;
-        
-                case BackgroundStyles.Darken:
-                    EditorGUI.DrawRect (rect, DARKEN_COLOUR);
-                    break;
-        
-                case BackgroundStyles.Lighten:
-                    EditorGUI.DrawRect (rect, LIGHTEN_COLOUR);
-                    break;
-                }
+            private static void DrawScriptReference(Rect rect, SerializedProperty property){
+                EditorGUI.BeginDisabledGroup (true);
+                EditorGUI.PropertyField (rect, property, false);
+                EditorGUI.EndDisabledGroup ();
             }
         }
     
